1d2c3f0a2365051d16ec0d25af3c82c599615474
Bartuer <bazhou@microsoft.com>
2021-08-06 10:40:26 +0000

    test time util

--- qlibc/test/tsidx.c
+++ qlibc/test/tsidx.c
@@ -4,22 +4,40 @@
 #include <stdio.h>
 #endif // _WIN32
 
+#include "cmdopt.h"
+#include "picojson.h"
 #include <cstdlib>
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <time.h>
 #include <tsidx.h>
 
-#include "cmdopt.h"
+inline time_t str2time_t(const char *time_str) {
+  struct tm tm;
+  strptime(time_str, "%Y-%m-%d %H:%M:%S", &tm);
+  return mktime(&tm);
+}
+
+inline char *time_t2str(const time_t t, char *time_str) {
+  memset(time_str, 0, 20);
+  std::strftime(time_str, 20, "%Y-%m-%d %H:%M:%S", std::localtime(&t));
+  return time_str;
+}
 
 namespace {
 int param_num = 7;
+const char *data_filename = NULL;
+const char *case_filename = NULL;
 void print_help(const char *cmd) {
   std::cerr << "Usage: " << cmd
             << " [OPTION]... \n\n"
                "Options:\n"
                "  -n, --num            number\n"
                "  -h, --help           print this help\n"
+               "  -d, --data           time series data file name\n"
+               "  -c, --test_case      test case file name\n"
             << std::endl;
 }
 
@@ -28,10 +46,13 @@ void print_help(const char *cmd) {
 int main(int argc, char *argv[]) {
   std::ios::sync_with_stdio(false);
 
-  ::cmdopt_option long_options[] = {
-      {"num", 1, NULL, 'n'}, {"help", 0, NULL, 'h'}, {NULL, 0, NULL, 0}};
+  ::cmdopt_option long_options[] = {{"num", 1, NULL, 'n'},
+                                    {"data", 1, NULL, 'd'},
+                                    {"case", 1, NULL, 'd'},
+                                    {"help", 0, NULL, 'h'},
+                                    {NULL, 0, NULL, 0}};
   ::cmdopt_t cmdopt;
-  ::cmdopt_init(&cmdopt, argc, argv, "n:tbwlc:o:xvgh", long_options);
+  ::cmdopt_init(&cmdopt, argc, argv, "n:d:c:h", long_options);
   int label;
   while ((label = ::cmdopt_get(&cmdopt)) != -1) {
     switch (label) {
@@ -46,6 +67,14 @@ int main(int argc, char *argv[]) {
       param_num = (int)value;
       break;
     }
+    case 'd': {
+      data_filename = cmdopt.optarg;
+      break;
+    }
+    case 'c': {
+      case_filename = cmdopt.optarg;
+      break;
+    }
     case 'h': {
       print_help(argv[0]);
       return 0;
@@ -53,5 +82,40 @@ int main(int argc, char *argv[]) {
     default: { return 1; }
     }
   }
-  printf("num :%d\n", param_num);
+
+  std::ifstream time_series(data_filename);
+  picojson::value v;
+  time_series >> v;
+  if (time_series.fail()) {
+    std::cerr << "JSON parse error -d " << data_filename << ", "
+              << picojson::get_last_error() << std::endl;
+    return -1;
+  }
+
+  std::vector<uint32_t> ts;
+  if (v.is<picojson::array>()) {
+    const picojson::array &a = v.get<picojson::array>();
+    size_t sz = a.size();
+    ts.reserve(sz);
+    for (picojson::array::const_iterator i = a.begin(); i != a.end(); ++i) {
+      ts.push_back(i->u_.number_);
+    }
+  }
+
+  std::ifstream time_case(case_filename);
+  picojson::value c;
+  time_case >> c;
+  if (time_case.fail()) {
+    std::cerr << "JSON parse error -c " << case_filename << ", "
+              << picojson::get_last_error() << std::endl;
+    return -1;
+  }
+
+  std::cout << "ts.size(): " << ts.size() << std::endl;
+  std::cout << str2time_t("2020-01-02 09:30:00") << ", " << 1577957400
+            << std::endl;
+
+  time_t t = 1577957400;
+  char tfstr[20];
+  std::cout << time_t2str(t, tfstr) << std::endl;
 }
