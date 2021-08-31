b1eb4b27a8c007c629a3dd350da8c607496817ae
Bartuer <bazhou@microsoft.com>
2021-08-31 10:36:38 +0000

    update MustelasQuote

--- t.py
+++ t.py
@@ -71,9 +71,11 @@ def d2j(a, name):
               indent=4) 
     
 class MustelasQuote(BaseQuote):
-    def __init__(self, df_file: str):
-        quote_df = pd.read_pickle(df_file)
+    def __init__(self, quote_df: pd.DataFrame):
+        quote_df = pd.read_pickle("data/quote_df.pkl")
+        print(quote_df)
         super().__init__(quote_df=quote_df)
+
         self.i = Tsidx()        # index
         self.d = {}             # cache
         self.n = {}             # name
@@ -96,48 +98,55 @@ class MustelasQuote(BaseQuote):
 
     def idx(self, i, b, e, f):
         j, k = t2i([b,e])
-        print(f"j,k:{j},{k},idx:{self.i[self.n[i]][j:k]}")
         return self.d[i][self.c[f]][self.i[self.n[i]][j:k]]
             
+    def end(self, i, e, f):
+        k = t2i([e])[0]
+        return self.d[i][self.c[f]][self.i[self.n[i]][:k]]
+    
     def beg(self, i, b, f):
         j = t2i([b])[0]
         return self.d[i][self.c[f]][self.i[self.n[i]][j:]]
 
-    def end(self, i, e, f):
-        k = t2i([e])[0]
-        return self.d[i][self.c[f]][self.i[self.n[i]][:k]]
-
-    def get_data(self, stock_id, start_time, end_time, field, method):
-        if (method == 'last'):  # number
-            return self.end(stock_id, end_time, field)
-        elif (method == 'sum'): # number
-            return self.idx(stock_id, start_time, end_time, field).sum()
-        elif (method == 'mean'): # number
-            return np.mean(self.idx(stock_id, start_time, end_time, field))
-        elif (method == 'all'): # bool
-             if field == 'limit_sell':
-                 return self.s[stock_id].size == 0
-             if field == 'limit_buy':
-                 return self.b[stock_id].size == 0
-             return self.s[stock_id].size == 0 or self.b[stock_id].size == 0
-        elif (method == "None"): # series
-            return n2d(self.idx(stock_id, start_time, end_time, field))
-        elif (method == 'any'): # exception
+    def get_data(self, stock_id, start_time, end_time, fields=None, method=None):
+        if (fields is None and method is None):
+            j, k = t2i([start_time,end_time])
+            i = self.i[self.n[stock_id]]
+            b = i.start
+            e = i.stop
+            if (b <= j <= k <= e):
+                return 0
+            else:
+                return None
+        elif (method == 'all'):   # 38%, bool
+            if (fields == 'limit_sell'):
+                 return self.s[stock_id].size != 0
+            if (fields == 'limit_buy'):
+                 return self.b[stock_id].size != 0
+            return self.s[stock_id].size != 0 or self.b[stock_id].size != 0
+        elif (callable(method)):  # 49%, number
+            return self.beg(stock_id, start_time, fields)
+        elif (method == 'last'):  # 0%, number
+            return self.end(stock_id, end_time, fields)
+        elif (method == 'sum'):   # 0%, number
+            return self.idx(stock_id, start_time, end_time, fields).sum()
+        elif (method == 'mean'):  # 0%, number
+            return np.mean(self.idx(stock_id, start_time, end_time, fields))
+        elif (method is None):    # 13%, pd series
+             return n2d(self.idx(stock_id, start_time, end_time, fields))
+        elif (method == 'any'):   # exception
             raise NotImplementedError("not implement")
         else:
-            return n2d(self.idx(stock_id, start_time, end_time, field))
+            raise ValueError("fields and method should be str, and method should be one of: last, sum, mean, all, None")
         return 
-
 
 q = MustelasQuote("data/quote_df.pkl")
 print(f"\
-all:{q.get_data('SH600004','2020-05-30','2020-06-12','$volume','all')}\n\
-all:{q.get_data('SH600004','2020-05-30','2020-06-12','limit_buy','all')}\n\
 sum:{q.get_data('SH600004','2020-05-30','2020-06-12','$volume','sum')}\n\
 mean:{q.get_data('SH600004','2020-05-30','2020-06-12','$volume','mean')}\n\
 last:{q.get_data('SH600004','2020-05-30','2020-06-12','$volume','last')}\n\
-{q.get_data('SH600004','2020-06-11','2020-06-12','$volume','None')}\n\
-{q.get_data('SH600004','2020-06-11','2020-06-12','$close','None')}\n\
-{q.get_data('SH600000','2020-01-02 09:31:00', '2020-01-02 09:31:59', '$close', 'None')}\n\
+{q.get_data('SH600004','2020-06-11','2020-06-12','$volume')}\n\
+{q.get_data('SH600004','2020-06-11','2020-06-12','$close')}\n\
+{q.get_data('SH600000','2020-01-02 09:31:00', '2020-01-02 09:31:59', '$close')}\n\
 ")
 
