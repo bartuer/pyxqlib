e41f507bfc26ffe19e77b4b8930ca5bdc6588a9c
Bartuer <bazhou@microsoft.com>
2021-09-06 16:14:13 +0000

    add copy lib

--- qlibc/deploy/push_lib.sh
+++ qlibc/deploy/push_lib.sh
@@ -1,2 +1,3 @@
 scp /home/bazhou/local/src/pyxqlib/build/lib.linux-x86_64-3.7/pyxqlib.cpython-37m-x86_64-linux-gnu.so qlib:/home/shared_user/anaconda3/envs/bazhou/lib/python3.7/site-packages/pyxqlib-0.0.1-py3.7-linux-x86_64.egg/pyxqlib.cpython-37m-x86_64-linux-gnu.so
 scp /home/bazhou/local/src/pyxqlib/build/lib.linux-x86_64-3.7/pyxqlib.cpython-37m-x86_64-linux-gnu.so qlib:/home/shared_user/bazhou/pyxqlib/py37env/lib/python3.7/site-packages/pyxqlib-0.0.1-py3.7-linux-x86_64.egg/pyxqlib.cpython-37m-x86_64-linux-gnu.so
+cp /home/bazhou/local/src/pyxqlib/build/lib.linux-x86_64-3.7/pyxqlib.cpython-37m-x86_64-linux-gnu.so /home/bazhou/local/src/nested_profile/conda_bazhou/lib/python3.7/site-packages/pyxqlib-0.0.1-py3.7-linux-x86_64.egg/pyxqlib.cpython-37m-x86_64-linux-gnu.so
